072c64df92aaf510beee46bff715aa908f692e1a
=Premkumar Savale <premsavle112@gmail.com>
2026-01-01 23:14:12 +0530

    Added Code

---
+++ FundaMental.js
@@ -0,0 +1,43 @@
+
+ const data = require('./data');
+  
+  console.log(data);
+   
+
+
+
+// var a = 200;
+
+// if (a == 200) {
+//     console.log("true")
+
+// }
+// else {
+//     console.log("Else")
+// }
+
+
+// function Fruit(item)
+// {
+//      console.log(item)
+// }
+
+// Fruit("banana");
+ 
+
+ 
+//  for(var a =2; a<=20; a=a+2)
+//  {
+//      console.log(a);
+      
+//  }
+
+//  var a = ["Vaibhav", "Premkumar", "Ajay", "Amol"];
+  
+// for(let i=0; i<a.length; i++)
+// {
+//      console.log(a[i]) ;
+      
+// }
+   
+ 
---
+++ Simple.js
@@ -0,0 +1,21 @@
+
+
+
+ const fs = require("fs");
+  
+  fs.writeFileSync("prem.txt", "Welcome to Node js ");
+   
+// // console.log(2 + 2);
+
+
+// // console.log("Step by step ...");
+
+
+
+//  function Apple(item) 
+//  {
+//      console.log(item)
+//  };
+  
+//   Apple("Data");
+   
