9dee5e278ef257a4ffb297888945de38c9fc4fff
=Premkumar Savale <premsavle112@gmail.com>
2026-01-04 10:29:56 +0530

    Added Code

---
+++ fsCrud.js
@@ -0,0 +1,29 @@
+
+const fs = require('fs');
+
+
+ console.log(process.argv); 
+  
+  
+  
+// fs.writeFileSync("files/apple.txt", "This is a Fruit");
+
+
+//  fs.writeFileSync("files/banan.txt", "This is a Fruit");
+
+
+// fs.unlinkSync("files/banan.txt");
+ 
+// const  data = fs.readFileSync("files/apple.txt", 'utf-8');
+ 
+//  const  data_1 = fs.readFileSync("files/banan.txt", 'utf-8');
+  
+//  console.log(data);
+  
+//   console.log(data_1);
+  
+
+//  fs.appendFileSync('files/apple.txt', "Welcome to Node js Technology by Updated By.....");
+  
+//    const data = fs.readFileSync("files/apple.txt", 'utf-8');
+//     console.log(data)
